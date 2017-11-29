94d5bd17e2011e5f63e00fd1f7fcc8e25da7573b
Andreas Fink <spam_social@afink.at>
2017-11-29 14:35:34 +0100

    changed api url

--- src/functions/Requests.js
+++ src/functions/Requests.js
@@ -1,5 +1,5 @@
 export const postRequest = (cb) => {
-    fetch('http://10.51.50.27/AUT_SFF/get.php', {
+    fetch('https://aut-sff.afink.at/backend/', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
